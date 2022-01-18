1d6432aa049fe9f1ae84965511979f92e5abc8d5
aremizov <aremizov@aori.ru>
2022-01-18 18:57:40 +0400

    initial commit

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,51 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    private int index;
 
     void clear() {
+        Arrays.fill(storage, 0, index, null);
+        index = 0;
     }
 
     void save(Resume r) {
+        storage[index] = r;
+        index++;
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < index; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                return storage[i];
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < index; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                while (i != index) {
+                    storage[i] = storage[i + 1];
+                    i++;
+                }
+            }
+        }
+        index--;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOf(storage, index);
     }
 
     int size() {
-        return 0;
+        return index;
     }
 }
